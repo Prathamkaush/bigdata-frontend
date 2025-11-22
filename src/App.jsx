341b0465e944a74ba269267d55f4d0cccbc27b00
Prathamkaush <prathamkaushik2002@gmail.com>
2025-11-22 16:22:33 +0530

    versel app.jsx v-1.1

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/auth/Login.jsx";
 import ProtectedLayout from "./components/layout/ProtectedLayout";
 import Dashboard from "./pages/dashboard/Dashboard.jsx";
-import CreateUser from "./pages/users/createuser.jsx";
+import CreateUser from "./pages/users/Createuser.jsx";
 import Credits from "./pages/credits/Credits.jsx";
-import Logs from "./pages/logs/Logs.jsx";
-import StatsPage from "./pages/stats/statsPage.jsx";
-import Settings from "./pages/settings/settings.jsx";
+import Logs from "./pages/logs/Logger.jsx";
+import StatsPage from "./pages/stats/StatsPage.jsx";
+import Settings from "./pages/settings/Settings.jsx";
 
 export default function App() {
   return (
